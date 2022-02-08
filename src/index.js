dede9a1e47b6d67ea2d0ecf83ce9a124795d3c28
Your Name <you@example.com>
2022-02-09 00:33:31 +0330

    not Done , Refactore router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './main';
+import A from "./component/router/a";
+import B from "./component/router/b";
+import 'antd/dist/antd.css';
+import { BrowserRouter , Route,Routes} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <BrowserRouter>
+  <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="A" element={<A />} />
+      <Route path="B" element={<B />} />
+    </Routes>
+ </BrowserRouter>
+ ,
   document.getElementById('root')
 );
 
